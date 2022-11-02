eca0b40e5e4c0c0f5a1b8853aa0fac631fd13e5b
thakrisp <61070282+thakrisp@users.noreply.github.com>
2022-11-02 18:16:48 -0400

    change site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thakrisp.github.io",
+  site: "https://thakrisp.github.io/RCrispinResume",
   base: "/dist",
 
   integrations: [tailwind()],

f92d4be046de2e31e55f84b431d8aec7c563face
thakrisp <61070282+thakrisp@users.noreply.github.com>
2022-11-02 17:52:33 -0400

    yml updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://thakrisp.github.io",
+  base: "/",
+
   integrations: [tailwind()],
 });

773616e1d84d5b7f565bd53bc9692edf8845a6a9
thakrisp <61070282+thakrisp@users.noreply.github.com>
2022-11-02 17:55:26 -0400

    config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
